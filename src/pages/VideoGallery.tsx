451844551db6e9fb2c87260d2bf923709a36744f
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-12-08 20:22:19 +0300

    🛠️ src/pages/VideoGallery.tsx -> Added margin-top to div

--- src/pages/VideoGallery.tsx
+++ src/pages/VideoGallery.tsx
@@ -53,7 +53,7 @@ export const VideoGallery: FC = () => {
     : videos.filter(video => video.category === selectedCategory);
 
   return (
-    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 mt-16">
       <VideoHeader />
       <CategoryFilter
         categories={categories}
